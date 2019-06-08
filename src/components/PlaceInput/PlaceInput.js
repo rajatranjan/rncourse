12581cd534a5f16b1806f44111dd530830904adc
rajatranjan <rjt.ranjan@gmail.com>
2019-06-08 18:30:28 +0530

    vs code settings

--- src/components/PlaceInput/PlaceInput.js
+++ src/components/PlaceInput/PlaceInput.js
@@ -1,55 +1,56 @@
-import React, { Component } from 'react';
-import { View, TextInput, Button, StyleSheet } from 'react-native';
-
+import React, { Component } from "react";
+import { View, TextInput, Button, StyleSheet } from "react-native";
 
 class PlaceInput extends Component {
-    state = {
-        placeName: "",
-      };
-      placeNameChangedHandler = val => {
-        this.setState({
-          placeName: val,
-        });
-    }
+  state = {
+    placeName: ""
+  };
+  placeNameChangedHandler = val => {
+    this.setState({
+      placeName: val
+    });
+  };
 
-    placeSubmitHandler = () => {
-      if (this.state.placeName.trim() === "") { //trim function remove space before and after any string
-        return;
-      }
-      this.props.onPlaceAdded(this.state.placeName)
-    };
-
-    render() {
-        return (
-            <View style={styles.inputContainer}>  
-                <TextInput
-                    style={styles.placeInput}
-                    placeholder="an Awesome place"
-                    value={this.state.placeName}
-                    onChangeText={this.placeNameChangedHandler}
-                    />
-                <Button
-                title="ADD"
-                style={styles.placeButton}
-                onPress={this.placeSubmitHandler}/>
-            </View>
-        );
+  placeSubmitHandler = () => {
+    if (this.state.placeName.trim() === "") {
+      //trim function remove space before and after any string
+      return;
     }
+    this.props.onPlaceAdded(this.state.placeName);
+  };
+
+  render() {
+    return (
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.placeInput}
+          placeholder="an Awesome place"
+          value={this.state.placeName}
+          onChangeText={this.placeNameChangedHandler}
+        />
+        <Button
+          title="ADD"
+          style={styles.placeButton}
+          onPress={this.placeSubmitHandler}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    inputContainer: {
-        // flex: 1,
-        width:"100%",
-        flexDirection: 'row',
-        justifyContent: "space-between",
-        alignItems:"center"
-      },
-      placeInput: {
-        width: "70%"
-      },
-      placeButton: {
-        width: "30%"
-      }
+  inputContainer: {
+    // flex: 1,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  placeInput: {
+    width: "70%"
+  },
+  placeButton: {
+    width: "30%"
+  }
 });
 export default PlaceInput;
--- src/components/PlaceList/PlaceList.js
+++ src/components/PlaceList/PlaceList.js
@@ -1,27 +1,26 @@
-import React from 'react';
-import { FlatList, StyleSheet } from 'react-native';
-import ListItem from '../ListItem/ListItem';
-
+import React from "react";
+import { FlatList, StyleSheet } from "react-native";
+import ListItem from "../ListItem/ListItem";
 
 const PlaceList = props => {
-    
-    return <FlatList 
+  return (
+    <FlatList
       style={styles.listContainer}
       data={props.places}
-      renderItem={(info) => (
-        <ListItem 
+      renderItem={info => (
+        <ListItem
           placeName={info.item.name} //item is a property on the info object, that's where it's coming from.
           placeImage={info.item.image}
-          onItemPressed={() => props.onItemSelected(info.item.key) }
+          onItemPressed={() => props.onItemSelected(info.item.key)}
         />
-      )}/>;
+      )}
+    />
+  );
 };
 
 const styles = StyleSheet.create({
-
   listContainer: {
     width: "100%"
-
   }
-})
+});
 export default PlaceList;
