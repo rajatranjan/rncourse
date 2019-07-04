c9e511bbfe60b82bd651bc7562ace5abf5eafb33
rajatranjan <rjt.ranjan@gmail.com>
2019-07-05 00:01:06 +0530

    Tab Navigation with Icon

--- App.js
+++ App.js
@@ -1,94 +1,23 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import { Navigation } from "react-native-navigation";
 
-import React, { Component } from "react";
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  Button
-} from "react-native";
-import { connect } from "react-redux";
+import AuthScreen from "./src/screens/Auth/Auth";
+import SharePlaceScreen from "./src/screens/SharePlace/SharePlace";
+import FindPlaceScreen from "./src/screens/FindPlace/FindPlace";
+//Register Screens
+Navigation.registerComponent("awsome-places.AuthScreen", () => AuthScreen); //first argument is name identifier
+Navigation.registerComponent(
+  "awsome-places.SharePlaceScreen",
+  () => SharePlaceScreen
+);
+Navigation.registerComponent(
+  "awsome-places.FindPlaceScreen",
+  () => FindPlaceScreen
+);
 
-import PlaceInput from "./src/components/PlaceInput/PlaceInput";
-import PlaceList from "./src/components/PlaceList/PlaceList";
-import PlaceDetail from "./src/components/PlaceDetail/PlaceDetail";
-import {
-  addPlace,
-  deletePlace,
-  selectPlace,
-  deselectPlace
-} from "./src/store/actions/index";
-// import console = require("console");
-
-class App extends Component {
-  placeAddedHandler = placeName => {
-    this.props.onAddPlace(placeName);
-    console.log(`place Added ${placeName}`);
-  };
-
-  placeDeletedHandler = () => {
-    this.props.onDeletePlace();
-  };
-  modalClosedHandler = () => {
-    this.props.onDeselectPlace();
-  };
-  placeSelectedHandler = key => {
-    this.props.onSelectPlace(key);
-  };
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <PlaceDetail
-          selectedPlace={this.props.selectedPlace}
-          onItemDeleted={this.placeDeletedHandler}
-          onModalClosed={this.modalClosedHandler}
-        />
-        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
-        <PlaceList
-          places={this.props.places}
-          onItemSelected={this.placeSelectedHandler}
-        />
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-    justifyContent: "flex-start",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
+//start App, doc about it is online in react native navigation guide
+Navigation.startSingleScreenApp({
+  screen: {
+    screen: "awsome-places.AuthScreen",
+    title: "Login"
   }
 });
-
-const mapStateToProps = state => {
-  return {
-    places: state.places.places, //first 'places' is from rootReducer function of configureStore.js file, second 'places.js' is from reducers folder places.js
-    selectedPlace: state.places.selectedPlace
-  };
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    onAddPlace: name => dispatch(addPlace(name)), //it imported from actions/index.js
-    //we later use this onAddPlace in component and pass name of place to the function.
-    onDeletePlace: () => dispatch(deletePlace()),
-    onSelectPlace: key => dispatch(selectPlace(key)),
-    onDeselectPlace: () => dispatch(deselectPlace())
-  };
-};
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App);
